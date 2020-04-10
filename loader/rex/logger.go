113cedda191f37023652a48d2d7379ce8b97dc31
Bernhard Reitinger <br@rexos.org>
2020-04-10 13:17:26 +0200

    Integrate support for reading REX files (#1)
    For details please consult the open source REXfile format
    (https://github.com/roboticeyes/gorexfile)

---
+++ loader/rex/logger.go
@@ -0,0 +1,12 @@
+// Copyright 2020. Bernhard Reitinger. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rex
+
+import (
+	"github.com/g3n/engine/util/logger"
+)
+
+// Package logger
+var log = logger.New("REX", logger.Default)
